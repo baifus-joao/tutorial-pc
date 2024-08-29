5b0500b49759bb7073a28a7a444793b0d4d2e59e
baifus-joao <joaovsbaifus@gmail.com>
2024-08-29 00:05:00 -0300

    tutorial 04

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ref } from 'vue'
+  import { ref } from 'vue'
 
-defineProps({
-  event: {
-    type: Object,
-    required: true
-  }
-})
+  defineProps({
+    event: {
+      type: Object,
+      required: true
+    }
+  })
 </script>
 
 <template>
@@ -17,16 +17,16 @@ defineProps({
 </template>
 
 <style scoped>
-.event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
+  .event-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+  }
 
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
+  .event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
 </style>
